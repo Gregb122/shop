94245fe358655557ad321e2eeb9f99ea4eabd61e
Grzegorz Bielecki <grzegorz.bielecki9@gmail.com>
2020-06-17 03:26:43 +0200

    Clean project

---
+++ Catalog.API/Controllers/CatalogServiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogServiceController : ControllerBase
+    {
+        private readonly ILogger<CatalogServiceController> _logger;
+
+        public CatalogServiceController(ILogger<CatalogServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
